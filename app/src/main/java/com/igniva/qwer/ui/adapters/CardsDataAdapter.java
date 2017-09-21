feb9266f9ce3c218aad0e937957851998a899ceb
ignivajigyasa <jigyasa.github@ignivasolutions.com>
2017-09-21 11:33:54 +0530

    New Files -- Feature Home

---
+++ app/src/main/java/com/igniva/qwer/ui/adapters/CardsDataAdapter.java
@@ -0,0 +1,89 @@
+package com.igniva.qwer.ui.adapters;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.igniva.qwer.R;
+
+public class CardsDataAdapter extends ArrayAdapter<String> {
+
+    ViewPager viewPager;
+    Context context;
+    public CardsDataAdapter(Context context) {
+
+        super(context, R.layout.card_content);
+        this.context=context;
+    }
+    int[] mResources = {
+            R.drawable.busesgallery03,
+            R.drawable.busesgallery03,
+            R.drawable.busesgallery03,
+            R.drawable.busesgallery03,
+            R.drawable.busesgallery03,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher
+    };
+
+    @Override
+    public View getView(int position, final View contentView, ViewGroup parent){
+        TextView v = (TextView)(contentView.findViewById(R.id.content));
+        v.setText(getItem(position));
+
+        viewPager=(ViewPager)contentView.findViewById(R.id.view_pager);
+
+     CustomPagerAdapter   mCustomPagerAdapter = new CustomPagerAdapter(context);
+
+//        mViewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(mCustomPagerAdapter);
+        return contentView;
+    }
+    class CustomPagerAdapter extends PagerAdapter {
+
+        Context mContext;
+        LayoutInflater mLayoutInflater;
+
+        public CustomPagerAdapter(Context context) {
+            mContext = context;
+            mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return mResources.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == ((LinearLayout) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View itemView = mLayoutInflater.inflate(R.layout.pager_item, container, false);
+
+            ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            imageView.setImageResource(mResources[position]);
+
+            container.addView(itemView);
+
+            return itemView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((LinearLayout) object);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/igniva/qwer/ui/fragments/BaseFragment.java
@@ -0,0 +1,12 @@
+package com.igniva.qwer.ui.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by igniva-andriod-02 on 18/11/16.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    public abstract void setUpLayout();
+    public abstract void setDataInViewObjects();
+}
