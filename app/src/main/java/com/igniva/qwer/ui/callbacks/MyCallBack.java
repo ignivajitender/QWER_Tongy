1783dbd6b3d680b992442544b0b882bd3599cb2e
karanveer <karanveer.github@ignivasolutions.com>
2017-09-22 11:15:00 +0530

    New Screens

---
+++ app/src/main/java/com/igniva/qwer/ui/callbacks/MyCallBack.java
@@ -0,0 +1,9 @@
+package com.igniva.qwer.ui.callbacks;
+
+/**
+ * Created by karanveer on 21/9/17.
+ */
+
+public interface MyCallBack {
+    public void removeItemFromHashMap(String type,String value, int pos);
+}
