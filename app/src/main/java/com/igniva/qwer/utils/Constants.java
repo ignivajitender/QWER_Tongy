c274093ad105bbeac41280234b4d625a2abecb08
ignivajigyasa <jigyasa.github@ignivasolutions.com>
2017-09-22 11:05:01 +0530

    Login WalkThrough Splash Settings SignUp

---
+++ app/src/main/java/com/igniva/qwer/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.igniva.qwer.utils;
+
+/**
+ * Created by Rohit on 3/7/17.
+ */
+
+public class Constants {
+
+    public static final int CAMERA_CODE = 2344;
+    public static final int GALLERY_CODE = 2444;
+    public static final int CROPING_CODE = 2345;
+
+
+    public static final String IS_ALREADY_LOGIN = "IS_ALREADY_LOGIN";
+    public static final String IS_GUEST_USER = "IS_GUEST_USER";
+
+}
